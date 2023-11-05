9b3487c91b2123a9210d2b330172cb564a73a17b
Liren21 <Kuminovvv21@gmail.com>
2023-11-05 17:15:55 +0600

    upd

--- src/app/components/App/App.tsx
+++ src/app/components/App/App.tsx
@@ -1,12 +1,14 @@
 import {observer} from "mobx-react-lite"
 import './App.scss'
 import Router from "../Router/Router";
+import { ToastContainer } from "react-toastify";
 
 export default observer(() => {
 
 
     return (
         <div>
+            <ToastContainer/>
             <Router/>
         </div>
     )
--- src/app/components/pages/InstallationsOverview/InstallationsOverview.tsx
+++ src/app/components/pages/InstallationsOverview/InstallationsOverview.tsx
@@ -2,11 +2,13 @@ import {observer} from "mobx-react-lite";
 import {CalcPercentSeq} from "../../generic/Function/CalcPercentSeq";
 import CustomInput from "../../UI/CustomInput/CustomInput";
 import './InstallationsOverview.scss'
-import React, {useEffect} from "react";
+import React, {ChangeEvent, useCallback, useEffect} from "react";
 import installationsOverviewStateStore from "../../../lib/store/installations-overview-state-store";
 import Container from "../../UI/Container/Container";
 import {useLocalStorage} from 'usehooks-ts'
 import Info from "./Info/Info";
+import Toaster from "../../../../core/lib/toaster/toaster";
+import {toast} from "react-toastify";
 
 export default observer(() => {
     const [isDarkTheme, setDarkTheme] = useLocalStorage<any>('installationsOverviewStateStore', {})
@@ -17,25 +19,53 @@ export default observer(() => {
     useEffect(() => {
         CalcPercentSeq(Number(isDarkTheme.percent), Number(isDarkTheme.value), Number(isDarkTheme.initialValue))
     }, [isDarkTheme])
+    const onChangeValue = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        if (Number(e.target.value) < 0) {
+            new Toaster({msg: 'Число не может быть отрицательным', type: toast.TYPE.WARNING})
+        } else {
+            setDarkTheme({...isDarkTheme, value: e.target.value})
+        }
+    }, [isDarkTheme, setDarkTheme])
+    const onChangePercent = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        if (Number(e.target.value) < 0) {
+            new Toaster({msg: 'Число не может быть отрицательным', type: toast.TYPE.WARNING})
+        } else {
+            setDarkTheme({...isDarkTheme, percent: e.target.value})
+        }
+    }, [isDarkTheme, setDarkTheme])
 
+    const onChangeInitialValue = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        if (Number(e.target.value) < 0) {
+            new Toaster({msg: 'Число не может быть отрицательным', type: toast.TYPE.WARNING})
+        } else {
+            setDarkTheme({...isDarkTheme, initialValue: e.target.value})
+        }
+    }, [isDarkTheme, setDarkTheme])
+    const onChangeInstallationCost = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        if (Number(e.target.value) < 0) {
+            new Toaster({msg: 'Число не может быть отрицательным', type: toast.TYPE.WARNING})
+        } else {
+            setDarkTheme({...isDarkTheme, installationCost: e.target.value})
+        }
+    }, [isDarkTheme, setDarkTheme])
     return (
         <Container className='installations-overview' panelElement={<Info isDarkTheme={isDarkTheme}/>}>
             <div className='installations-overview__body'>
                 <div className='installations-overview__form'>
                     <CustomInput title={'Начальное число'}
-                                 onChange={(e) => setDarkTheme({...isDarkTheme, value: e.target.value})}
+                                 onChange={onChangeValue}
                                  value={isDarkTheme.value} type="number"
                                  className="input input-bordered w-full max-w-xs"/>
                     <CustomInput title={'Прогрессия'}
-                                 onChange={(e) => setDarkTheme({...isDarkTheme, percent: e.target.value})}
+                                 onChange={onChangePercent}
                                  value={isDarkTheme.percent} type="number"
                                  className="input input-bordered w-full max-w-xs"/>
                     <CustomInput title={'Количество установок'}
-                                 onChange={(e) => setDarkTheme({...isDarkTheme, initialValue: e.target.value})}
+                                 onChange={onChangeInitialValue}
                                  value={isDarkTheme.initialValue} type="number"
                                  className="input input-bordered w-full max-w-xs"/>
                     <CustomInput title={'Стоимость 1-ой установки'}
-                                 onChange={(e) => setDarkTheme({...isDarkTheme, installationCost: e.target.value})}
+                                 onChange={onChangeInstallationCost}
                                  value={isDarkTheme.installationCost} type="number"
                                  className="input input-bordered w-full max-w-xs"/>
                 </div>
